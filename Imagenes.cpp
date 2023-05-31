1d30e0746a078cbe04584563472c3d6075b08968
franciscosamaniego <102254119+franciscosamaniego@users.noreply.github.com>
2023-05-31 16:14:15 -0300

    Update Imagenes.cpp

--- Imagenes.cpp
+++ Imagenes.cpp
@@ -4,13 +4,13 @@ using namespace std;
 struct Imagen {
     string nombre;
     int ancho;
-    int alto;
+    int alto; //estructura de las imagenes
     double size;
 };
 
 void imprimirImagen(Imagen nuevaImagen[700], int cantI) {
     char n = 164;
-    for(int i = 0; i < cantI; i++) {
+    for(int i = 0; i < cantI; i++) { //funcion que imprime todas las imagenes ingresadas
         cout << "Imagen numero " << i+1 << endl;
         cout << "\tNombre: " << nuevaImagen[i].nombre << endl;
     }
@@ -21,7 +21,8 @@ int ingresarImagen(Imagen nuevaImagen[700], int cont) {
     int alto, ancho;
     double size;
     char n = 164;
-    cout << "Ingrese el nombre de la imagen" << endl;
+    cout << "Ingrese el nombre de la imagen" << endl; //funcion para ingresar la imagen. Se le pide al usuario el nombre de la imagen
+    //su altura y su ancho y su tamaño
     cin.ignore();
     fgets(nombre,100,stdin);
     cout << "Ingrese el ancho de la imagen(en pixeles)" << endl;
@@ -41,7 +42,8 @@ void buscarImagen(Imagen nuevaImagen[700], int cantC) {
     string choice;
     bool encontrado = 0;
     char n = 164;
-    cout << "Que imagen quiere buscar?" << endl;
+    cout << "Que imagen quiere buscar?" << endl; //esta funcion se encarga de buscar una imagen que su nombre tenga la palabra ingresada
+    //por el usuario, si ocurre eso, lo imprime
     cin >> choice;
     for(int i = 0; i < cantC; i++) {
         if(nuevaImagen[i].nombre.find(choice) != string::npos) {
@@ -64,7 +66,8 @@ int main(void) {
     cout << "1)Ver las imagenes" << endl;
     cout << "2)Agregar una imagen" << endl;
     cout << "3)Buscar una imagen" << endl;
-    cout << "4)Salir" << endl;
+    cout << "4)Salir" << endl; //esta parte se encarga de mostrale al usuario distintas opciones para hacer cosas con las imagenes
+        //Si decide ingresar imagenes despues de llegar a las 700, se le avisara que llego al maximo
     cin >> choice;
     switch(choice) {
         case 1:
