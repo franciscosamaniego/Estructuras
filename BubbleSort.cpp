fc834560d21595c349ee5bc2fe9b47ad37b4b4b3
franciscosamaniego <102254119+franciscosamaniego@users.noreply.github.com>
2023-05-31 15:35:11 -0300

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void swap(int a[], int b, int c) {
   int temp = a[b];
-  a[b] = a[c];
+  a[b] = a[c]; //funcion para cambiar de posicion del array los numeros
   a[c] = temp;
 }
 
@@ -13,7 +13,8 @@ void swap(int a[], int b, int c) {
 void bubble(int a[], int n) {
   for (int i = 0; i < n; i++) {
     for(int j = 0; j < n-1; j++) {
-      if(a[j] > a[j+1]) {
+      if(a[j] > a[j+1]) { //el bubble sort consiste en comparar el numero en el que esta posicionado y el siguiente, si el primero es
+        //mas grande que el segundo, se cambia y si no se queda asi y esto se hace hasta llegar al anteultimo para poder hacer la comparacion
         swap(a,j,j+1);
       }
     }
